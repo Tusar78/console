14f5becffd7cc8e6c5894a65d51cb6e1c027bf1e
Tusar <tusarahmed141@gmail.com>
2022-06-14 01:09:46 +0600

    check email validation using regular expression

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,15 @@
-const arr = ['1', '2', '3', '4', '5'];
-for (const ar of arr) {
-  console.log(ar);
+// const arr = ['1', '2', '3', '4', '5'];
+// for (const ar of arr) {
+//   console.log(ar);
+// }
+
+const customValidate = _ => {
+  const emailFilled = document.getElementById('email');
+  const email = emailFilled.value;
+  const regEx = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
+  if (email.match(regEx)) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
 }
