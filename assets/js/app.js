ccbe9f3a1a8ebb674e4cbb0f050236e801a13638
Tusar <tusarahmed141@gmail.com>
2022-06-15 00:03:13 +0600

    Explore slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,34 @@
 //   console.log(ar);
 // }
 
-const customValidate = _ => {
-  const emailFilled = document.getElementById('email');
-  const email = emailFilled.value;
-  const regEx = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
-  if (email.match(regEx)) {
-    console.log('true');
-  } else {
-    console.log('false');
+// const customValidate = _ => {
+//   const emailFilled = document.getElementById('email');
+//   const email = emailFilled.value;
+//   const regEx = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
+//   if (email.match(regEx)) {
+//     console.log('true');
+//   } else {
+//     console.log('false');
+//   }
+// }
+
+
+
+const images = [
+  './assets/images/pic-1.jpg',
+  './assets/images/pic-2.jpg',
+  './assets/images/pic-3.jpg'
+]
+
+const imgContainer = document.querySelector('.img-item');
+let imgIndex = 0;
+
+setInterval (() => {
+  if (imgIndex >= images.length) {
+    imgIndex = 0;
   }
-}
+
+  const imgUrl = images[imgIndex];
+  imgContainer.src = imgUrl;
+  imgIndex++;
+}, 1500)
